79ca41134a0021e9b98217ec6a2110cf7e46b29b
Klapptnot Z3 <klapptnot@gmail.com>
2025-12-07 09:17:54 -0300

    lift () ➜ ✨ rm nonsense

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,13 +1,14 @@
 -- config loader
 
---- @alias ZnvConfig {mapping:NvimMappingConfig; options:NvimOptionsConfig; plugins:NvimPluginsConfig}
+--- @alias ZnvConfig {mapping:NvimMappingConfig; plugins:NvimPluginsConfig}
+
+-- Just load options
+require ("config.options")
 
 --- @type ZnvConfig
 local main = {
   --- @return NvimMappingConfig
   mapping = require ("config.mapping"):new (),
-  --- @return NvimOptionsConfig
-  options = require ("config.options"):new (),
   --- @return NvimPluginsConfig
   plugins = require ("config.plugins"):new (),
 } -- Return all configs classes
